f80da882d85525386b873f6e3251f333e467ce45
Christopher Soriano <2832@holbertonschool.com>
2021-11-30 00:29:08 -0400

    Add Print string task folder

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        string sub = str.Substring(0,9);
+		Console.WriteLine("{0}{0}{0}\n{1}", str, sub);
+        }
+}
