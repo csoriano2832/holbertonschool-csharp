faf2a9ebf132846acad4f58ca7d3657b99f2486d
Christopher Soriano <2832@holbertonschool.com>
2021-11-29 23:17:06 -0400

    [Edit] Changed to old template style

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace writeline
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
