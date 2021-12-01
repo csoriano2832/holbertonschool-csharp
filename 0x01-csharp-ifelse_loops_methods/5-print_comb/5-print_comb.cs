36f594b386461ddb9810cf71c82309dd9fc83a7c
Christopher Soriano <2832@holbertonschool.com>
2021-11-30 23:49:44 -0400

    add task folder

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i;
+        for (i = 0; i < 99; i++)
+        {
+            Console.Write("{0:D}{1:D}, ", (i / 10), (i % 10));
+        }
+        Console.Write("{0:D}{1:D}\n", (i / 10), (i % 10));
+    }
+}
