1d688a7ce90e12a360ba98644a0f6940e1b20b21
Christopher Soriano <2832@holbertonschool.com>
2021-11-30 23:28:03 -0400

    add more task folders

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length > 0)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\");
+            }
+        }
+        Console.WriteLine();
+    }
+}
