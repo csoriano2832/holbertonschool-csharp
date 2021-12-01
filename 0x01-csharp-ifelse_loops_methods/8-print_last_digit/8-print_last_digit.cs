aef8395380ef4111dcee51d32ea2f1d66c97feb2
Christopher Soriano <2832@holbertonschool.com>
2021-11-30 22:58:53 -0400

    add more task folders

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit = number % 10;
+
+        if (lastDigit < 0)
+        {
+            lastDigit = lastDigit * -1;
+        }
+        Console.Write("{0:D}", lastDigit);
+        return(lastDigit);
+    }
+}
