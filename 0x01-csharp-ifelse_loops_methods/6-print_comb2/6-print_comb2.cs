c0dc1dda58e440f9e7c79530a6e5daff8f4c6361
Christopher Soriano <2832@holbertonschool.com>
2021-12-01 00:01:04 -0400

    last minute pushgit add 6-print_comb2.cs 6-print_comb2.csproj !

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i;
+        int j;
+
+        for (i = 0; i <= 8; i++)
+        {
+            for (j = 1; j <= 9; j++)
+            {
+                if (i != j && i < j)
+                {
+                    Console.Write("{0:D}", i);
+                    Console.Write("{0:D}, ", j);
+                }
+            }
+            if (i == 8 && j == 9)
+            {
+                Console.WriteLine("{0:D}{1:D}", i, j);
+            }
+        }
+    }
+}
