580af45dd5a67c057037ca043e117348accbf455
Christopher Soriano <2832@holbertonschool.com>
2021-12-01 00:03:29 -0400

    last minute fix

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -11,15 +11,15 @@ class Program
         {
             for (j = 1; j <= 9; j++)
             {
-                if (i != j && i < j)
+                if (i == 8 && j == 9)
+                {
+                    Console.WriteLine("{0:D}{1:D}", i, j);
+                }
+                else if (i != j && i < j)
                 {
                     Console.Write("{0:D}", i);
                     Console.Write("{0:D}, ", j);
                 }
-            }
-            if (i == 8 && j == 9)
-            {
-                Console.WriteLine("{0:D}{1:D}", i, j);
             }
         }
     }
