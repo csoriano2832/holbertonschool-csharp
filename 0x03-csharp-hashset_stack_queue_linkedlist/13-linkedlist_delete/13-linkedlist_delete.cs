5514f7034ece90408de2ebfc821e7287b4637d2f
Christopher Soriano <2832@holbertonschool.com>
2021-12-02 15:59:58 -0400

    add tests for edge cases

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -5,6 +5,11 @@ class LList
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
+        if ((myLList == null) || (index < 0) || (index > myLList.Count - 1))
+        {
+            return;
+        }
+
         LinkedListNode<int> node = myLList.First;
 
         for (int i = 0; i < index; i++)
