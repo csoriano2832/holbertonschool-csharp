a1bceb5b85c0529267f05528517958374639cc8d
Christopher Soriano <2832@holbertonschool.com>
2021-12-02 20:46:22 -0400

    delete node before returning value

--- 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -5,13 +5,15 @@ class LList
 {
     public static int Pop(LinkedList<int> myLList)
     {
+        if (myLList == null)
+            return (0);
+
         LinkedListNode<int> node = myLList.First;
         
-        if (node is null)
-        {
+        if (node == null)
             return (0);
-        }
 
+        myLList.RemoveFirst();
         return (node.Value);
     }
 }
