990576e38a46ec3dea8ac84173769bb1608f9a31
Christopher Soriano <2832@holbertonschool.com>
2022-01-03 13:21:16 -0400

    Add more case tests

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -17,12 +17,37 @@ namespace Tests
             int output = MyMath.Operations.Max(nums);
             Assert.That(output, Is.EqualTo(0));
         }
+
         [Test]
         public void TestMaxList()
         {
             List<int> nums = new List<int>() {98, -10, 0, 32, 972, 12, 1024, -727, -98, 50, 8};
             int output = MyMath.Operations.Max(nums);
             Assert.That(output, Is.EqualTo(1024));
+        }
+
+        [Test]
+        public void TestMaxAtEnd()
+        {
+            List<int> nums = new List<int>() {98, -10, 0, 32, 972, 12, -727, -98, 50, 8, 1024};
+            int output = MyMath.Operations.Max(nums);
+            Assert.That(output, Is.EqualTo(1024));
+        }
+
+        [Test]
+        public void TestMaxAtBeginning()
+        {
+            List<int> nums = new List<int>() {1024, 98, -10, 0, 32, 972, 12, -727, -98, 50, 8};
+            int output = MyMath.Operations.Max(nums);
+            Assert.That(output, Is.EqualTo(1024));
+        }
+
+        [Test]
+        public void TestNegativeNumberInList()
+        {
+            List<int> nums = new List<int>() {-20, -17, -8, -100, -1};
+            int output = MyMath.Operations.Max(nums);
+            Assert.That(output, Is.EqualTo(-1));
         }
     }
 }
