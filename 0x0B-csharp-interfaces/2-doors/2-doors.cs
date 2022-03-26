8a5890a0bc9d4728210dca3ccd5caa301c853210
Christopher Soriano <2832@holbertonschool.com>
2022-03-26 07:50:38 -0400

    Fix typo on interact message

--- 0x0B-csharp-interfaces/2-doors/2-doors.cs
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -38,6 +38,6 @@ class Door : Base, IInteractive
 
     public void Interact()
     {
-        Console.WriteLine($"You try to open the {name}. It's locked");
+        Console.WriteLine($"You try to open the {name}. It's locked.");
     }
 }
