f529d3b61162dbe86cbd2ccf1333c712e1772640
Christopher Soriano <2832@holbertonschool.com>
2022-03-26 07:42:54 -0400

    Add task files 0-3 and README

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name;
+
+    public override string ToString()
+    {
+        var type = this.GetType().Name;
+
+        return ($"{name} is a {type}");
+    }
+}
