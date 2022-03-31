948eebcbcacdc5866c025b43724df0dd6a05ce71
Christopher Soriano <2832@holbertonschool.com>
2022-03-31 14:35:06 -0400

    Add code to file

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace _0_universal_health
+///<summary></summary>
+public class Player
 {
-    class Program
+    private string name { get; set; }
+    private float maxHp { get; set;}
+    private float hp { get; set; }
+
+    ///<summary></summary>
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        static void Main(string[] args)
+        this.name = name;
+        if (maxHp > 0)
         {
-            Console.WriteLine("Hello World!");
+            this.maxHp = maxHp;
         }
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        this.hp = this.maxHp;
+    }
+
+    ///<summary></summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
     }
 }
