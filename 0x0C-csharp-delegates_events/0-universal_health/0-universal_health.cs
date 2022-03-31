a31bc7e453a4a6e3989d20e3577731f400cd9466
Christopher Soriano <2832@holbertonschool.com>
2022-03-31 14:42:35 -0400

    Add XML documentation

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
-///<summary></summary>
+///<summary>This class represents a Player object.</summary>
 public class Player
 {
     private string name { get; set; }
     private float maxHp { get; set;}
     private float hp { get; set; }
 
-    ///<summary></summary>
+    ///<summary>Default constructor method.</summary>
     public Player(string name = "Player", float maxHp = 100f)
     {
         this.name = name;
@@ -23,7 +23,7 @@ public class Player
         this.hp = this.maxHp;
     }
 
-    ///<summary></summary>
+    ///<summary>Prints the current health of the player.</summary>
     public void PrintHealth()
     {
         Console.WriteLine($"{name} has {hp} / {maxHp} health");
